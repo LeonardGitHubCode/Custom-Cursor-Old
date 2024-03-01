1d730f26e46b7f327cb2559f269ac0306c9ae52c
LeonardGitHubCode <150854722+LeonardGitHubCode@users.noreply.github.com>
2024-03-01 20:17:28 +0100

    RemoveUpdate.bat

--- RemoveUpdate.bat
+++ RemoveUpdate.bat
@@ -3,13 +3,13 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 echo Welcome To Remove Update in Custom Cursor!
 timeout /t 3 /nobreak > nul
-cls
 
-set Update=
+
+set Update=removal
 set /p UpdateRemover=Are you sure to remove update?: 
 set "RemoveUpdate=%RemoveUpdate%:"
 
-                            echo Update not removed!
+                            echo Update not reemoval!
                                                       timeout /t 1 /nobreak > nul
 cls
 echo Removal complete!
